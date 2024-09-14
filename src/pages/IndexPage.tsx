8d5fbe32c426b57c24dfb6e54e65eee2f4336fc7
MRyutaro <m2002r1028@icloud.com>
2024-09-14 15:33:35 +0900

    update: 横スクロール

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,13 +1,64 @@
+import { useCallback, useEffect, useRef } from "react";
+
 import { Game1, Game2, Game3 } from "../components";
 
 export function IndexPage(): JSX.Element {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const updateScroll = useCallback(() => {
+        if (containerRef.current) {
+            // スクロール位置に応じて横に移動
+            console.log(window.scrollY);
+            containerRef.current.scrollLeft = window.scrollY;
+        }
+    }, []);
+
+    useEffect(() => {
+        // scrollで発火
+        window.addEventListener("scroll", updateScroll);
+
+        return () => {
+            window.removeEventListener("scroll", updateScroll);
+        };
+    }, [updateScroll]);
+
     return (
-        <div>
-            <h1>Index Page</h1>
-            <p>This is the index page.</p>
-            <Game1 />
-            <Game2 />
-            <Game3 />
+        <div
+            style={{
+                height: "100vh",
+                overflow: "hidden",
+                whiteSpace: "nowrap",
+            }}
+        >
+            <div
+                ref={containerRef}
+                style={{
+                    display: "flex",
+                    overflowX: "auto",
+                    whiteSpace: "nowrap",
+                    width: "100%",
+                    height: "100%",
+                    // スクロールバーを非表示
+                    scrollbarWidth: "none",
+                    msOverflowStyle: "none",
+                }}
+            >
+                <div style={{ minWidth: "100vw" }}>
+                    <h1>Index Page</h1>
+                </div>
+                <div style={{ minWidth: "100vw" }}>
+                    <Game1 />
+                </div>
+                <div style={{ minWidth: "100vw" }}>
+                    <Game2 />
+                </div>
+                <div style={{ minWidth: "100vw" }}>
+                    <Game3 />
+                </div>
+                <div style={{ minWidth: "100vw" }}>
+                    <h1>End Page</h1>
+                </div>
+            </div>
         </div>
     );
 }
