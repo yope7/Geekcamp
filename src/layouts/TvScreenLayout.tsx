683fb628b7d88246c72156563f87f44b1c11dc1b
MRyutaro <m2002r1028@icloud.com>
2024-09-14 22:58:13 +0900

    update: そとだし

---
+++ src/layouts/TvScreenLayout.tsx
@@ -0,0 +1,46 @@
+import { Typewriter } from "react-simple-typewriter";
+
+export function TvScreenLayout({ children }: { children: React.ReactNode }): JSX.Element {
+    return (
+        <div
+            style={{
+                width: "400px",
+                height: "300px",
+                alignItems: "center",
+                display: "flex",
+                justifyContent: "center",
+                perspective: "1000px", // 大きいほうが平面に近づく。多分目からオブジェクトまでの距離
+            }}
+        >
+            <div
+                style={{
+                    // この要素の設定
+                    width: "100%",
+                    height: "300px",
+                    padding: "10px",
+                    // 子要素の設定
+                    display: "flex",
+                    flexDirection: "column", // 子要素を縦に並べる
+                    // 3Dの設定
+                    transform: "rotateX(0deg) rotateY(10deg) rotateZ(-5deg)", // x, y, zの回転角度。各軸に対して回転
+                    translate: "40px 0px 200px", // 多分x, y, zの移動量
+                    transformOrigin: "-30%",
+                    // 背景の設定
+                    backgroundColor: "black",
+                    // テキストの設定
+                    fontFamily: "Courier New",
+                    color: "#00ffff",
+                    // スクロールできるように
+                    overflow: "auto",
+                    scrollbarWidth: "none",
+                    msOverflowStyle: "none",
+                    // 自動改行を有効に
+                    whiteSpace: "normal", // テキストを折り返す
+                    wordWrap: "break-word", // 長い単語も折り返す
+                }}
+            >
+                {children}
+            </div>
+        </div>
+    );
+}
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,25 +1,6 @@
-import { useCallback, useEffect, useRef } from "react";
-import { Typewriter } from "react-simple-typewriter";
+import { TvScreenLayout } from "../layouts/TvScreenLayout";
 
 export function IndexPage(): JSX.Element {
-    const containerRef = useRef<HTMLDivElement>(null);
-
-    const updateScroll = useCallback((event: WheelEvent) => {
-        // width
-        if (containerRef.current) {
-            containerRef.current.scrollLeft += event.deltaY;
-        }
-    }, []);
-
-    useEffect(() => {
-        // scrollで発火
-        window.addEventListener("wheel", updateScroll);
-
-        return () => {
-            window.removeEventListener("wheel", updateScroll);
-        };
-    }, [updateScroll]);
-
     return (
         <div
             style={{
@@ -40,7 +21,6 @@ export function IndexPage(): JSX.Element {
                 }}
             />
             <div
-                ref={containerRef}
                 style={{
                     display: "flex",
                     overflowX: "auto",
@@ -60,52 +40,9 @@ export function IndexPage(): JSX.Element {
                         justifyContent: "center",
                     }}
                 >
-                    <div
-                        style={{
-                            width: "400px",
-                            height: "300px",
-                            alignItems: "center",
-                            display: "flex",
-                            justifyContent: "center",
-                            perspective: "1000px", // 大きいほうが平面に近づく。多分目からオブジェクトまでの距離
-                        }}
-                    >
-                        <div
-                            style={{
-                                // この要素の設定
-                                width: "100%",
-                                height: "300px",
-                                padding: "10px",
-                                // 子要素の設定
-                                display: "flex",
-                                flexDirection: "column", // 子要素を縦に並べる
-                                // 3Dの設定
-                                transform: "rotateX(0deg) rotateY(10deg) rotateZ(-5deg)", // x, y, zの回転角度。各軸に対して回転
-                                translate: "40px 0px 200px", // 多分x, y, zの移動量
-                                transformOrigin: "-30%",
-                                // 背景の設定
-                                backgroundColor: "black",
-                                // テキストの設定
-                                fontFamily: "Courier New",
-                                color: "#00ffff",
-                                // スクロールできるように
-                                overflow: "auto",
-                                scrollbarWidth: "none",
-                                msOverflowStyle: "none",
-                                // 自動改行を有効に
-                                whiteSpace: "normal", // テキストを折り返す
-                                wordWrap: "break-word", // 長い単語も折り返す
-                            }}
-                        >
-                            <Typewriter words={["Welcome to the Retro World1!"]} cursor cursorStyle="_" typeSpeed={50} deleteSpeed={50} delaySpeed={1000} />
-                            <Typewriter words={["Welcome to the Retro World2!"]} cursor cursorStyle="_" typeSpeed={50} deleteSpeed={50} delaySpeed={1000} />
-                            <Typewriter words={["Welcome to the Retro World3!"]} cursor cursorStyle="_" typeSpeed={50} deleteSpeed={50} delaySpeed={1000} />
-                            <Typewriter words={["Welcome to the Retro World4!"]} cursor cursorStyle="_" typeSpeed={50} deleteSpeed={50} delaySpeed={1000} />
-                            <Typewriter words={["Welcome to the Retro World5!"]} cursor cursorStyle="_" typeSpeed={50} deleteSpeed={50} delaySpeed={1000} />
-                            <Typewriter words={["Welcome to the Retro World6!"]} cursor cursorStyle="_" typeSpeed={50} deleteSpeed={50} delaySpeed={1000} />
-                            <Typewriter words={["Welcome to the Retro World7!"]} cursor cursorStyle="_" typeSpeed={50} deleteSpeed={50} delaySpeed={1000} />
-                        </div>
-                    </div>
+                    <TvScreenLayout>
+                        Game 1
+                    </TvScreenLayout>
                 </div>
             </div>
         </div>
